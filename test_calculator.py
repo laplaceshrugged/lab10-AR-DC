f31a0519acdc93cbbc3679374792bf2d901ec8ad
laplaceshrugged <adityaa.ravi@gmail.com>
2025-11-16 17:32:44 -0500

    tests p1

--- test_calculator.py
+++ test_calculator.py
@@ -10,13 +10,17 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    ####### Partner 1
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(4,6), 24)
+        self.assertEqual(mul(-2,0), 0)
+        self.assertEqual(mul(-6,-7), 42)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(2,10), 5)
+        self.assertEqual(div(-2,0),0)
+        self.assertEqual(div(-7,-91),13)
+    ##########################
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,23 +37,28 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    ####### Partner 1
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
+        with self.assertRaises(ValueError):
+            logarithm(7,-343)
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(-3,-4),5)
+        self.assertEqual(hypotenuse(0,14),14)
+        self.assertEqual(hypotenuse(-40,9),41)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        with self.assertRaises(ValueError): 
+            square_root(-4761)
+        self.assertEqual(square_root(0),0)
+        self.assertEqual(square_root(1681),41)
+    #########################
 
 # Do not touch this
 if __name__ == "__main__":
